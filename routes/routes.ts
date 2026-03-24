1aef9a71de8c4d6209dc50a49567d4b1503fe888
David Páez <142960018+Dakewer@users.noreply.github.com>
2026-03-24 13:43:17 -0600

    pushing existing files

---
+++ routes/routes.ts
@@ -0,0 +1,53 @@
+"use strict";
+
+import express from "express";
+import path from "path";
+
+const router = express.Router()
+// res.send('ok')
+router.get("/", (req, res) => {
+    // res.send('ok')
+    res.render("home");
+});
+
+router.get("/login", (req, res) => {
+    res.render("login");
+})
+
+router.post("/login", (req, res) => {
+    //PENDIENTE
+})
+
+router.get("/signin", (req, res) => {
+    res.render("signin");
+})
+
+router.post("/signin", (req, res) => {
+
+})
+
+router.get("/calendar", (req, res) => {
+    res.render("calendar");
+})
+
+router.get("/profile", (req, res) => {
+    res.render("profile");
+})
+
+router.get("/clan", (req, res) => {
+    res.render("clan");
+})
+
+router.get("/add", (req, res) => {
+    res.render("add");
+})
+
+router.get("/todo", (req, res) => {
+    res.render("todo");
+})
+
+router.get("/settings", (req, res) => {
+    res.render("settings");
+})
+
+export default router;
