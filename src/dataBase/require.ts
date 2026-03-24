092ab10aa1ed5834d0459598ee639681fc8a935a
Diego <diego.orozcom@iteso.mx>
2026-03-24 14:21:30 -0600

    Base de datos

---
+++ src/dataBase/require.ts
@@ -0,0 +1,52 @@
+import mongoose, {
+    Schema
+} from "mongoose";
+
+const studentsSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    mail: {
+        type: String,
+        required: true,
+        match: /@/
+    },
+    expedient: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    semester: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 10
+    }
+});
+
+const Alumno = mongoose.model("students", studentsSchema);
+
+// taer
+export const getStudents = async () => {
+    return await Alumno.find({});
+};
+
+// eliminar
+export const deleteStudent = async (expedient: number) => {
+    return await Alumno.deleteOne({
+        expedient
+    });
+};
+
+// Crear o actualizar
+export const bothStudent = async (studentData: any) => {
+    return await Alumno.findOneAndUpdate({
+            expedient: studentData.expedient
+        },
+        studentData, {git push origin diego
+            upsert: true,
+            new: true
+        }
+    );
+};
